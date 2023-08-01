a31d3c69ca29b560f890c465bdc272d2c66ff93f
Tonystark64 <1183694330@qq.com>
2023-08-01 09:44:25 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ set -x
 port=$(shuf -i25000-30000 -n1)
 
 # for full parameter fine-tuning using LOMO
-deepspeed --master_port "$port" --include localhost:0 src/train_lomo.py config/args_lomo.yaml
+deepspeed --master_port "$port" --include localhost:0,1 src/train_lomo.py config/args_lomo.yaml
 
 # for LoRA + LOMO
 #deepspeed --master_port "$port" --include localhost:0 src/train_lomo_lora.py config/args_lomo_lora.yaml
